301d5251688e1b4edbf8274cd3be4a3a96ef0534
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-06-20 13:12:37 +0200

    Import intertia tensor code.

---
+++ src/main/java/net/imglib2/mesh/alg/InertiaTensor.java
@@ -0,0 +1,134 @@
+package net.imglib2.mesh.alg;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.linear.BlockRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import net.imglib2.RealLocalizable;
+import net.imglib2.mesh.MeshShapeDescriptors;
+import net.imglib2.mesh.obj.Mesh;
+import net.imglib2.mesh.obj.Triangle;
+
+/**
+ * Inertia tensor of a 3D mesh.
+ * <p>
+ * Based on <a href="https://thescipub.com/abstract/jmssp.2005.8.11"> Tonon, F.
+ * “Explicit Exact Formulas for the 3-D Tetrahedron Inertia Tensor in Terms of
+ * Its Vertex Coordinates.” Journal of Mathematics and Statistics 1, no. 1
+ * (March 31, 2005): 8–11. https://doi.org/10.3844/jmssp.2005.8.11.</a>
+ * <p>
+ * Adapted from the <code>DefaultInertiaTensor3DMesh</code> in
+ * <code>imagej-ops</code> by Tim-Oliver.
+ * 
+ * @author Tim-Oliver Buchholz (University of Konstanz)
+ */
+public class InertiaTensor
+{
+
+	public static RealMatrix calculate( final Mesh input )
+	{
+		final RealLocalizable cent = MeshShapeDescriptors.centroid( input );
+		final double originX = cent.getDoublePosition( 0 );
+		final double originY = cent.getDoublePosition( 1 );
+		final double originZ = cent.getDoublePosition( 2 );
+
+		BlockRealMatrix tensor = new BlockRealMatrix( 3, 3 );
+		for ( final Triangle triangle : input.triangles() )
+		{
+			final double x1 = triangle.v0x() - originX;
+			final double y1 = triangle.v0y() - originY;
+			final double z1 = triangle.v0z() - originZ;
+			final double x2 = triangle.v1x() - originX;
+			final double y2 = triangle.v1y() - originY;
+			final double z2 = triangle.v1z() - originZ;
+			final double x3 = triangle.v2x() - originX;
+			final double y3 = triangle.v2y() - originY;
+			final double z3 = triangle.v2z() - originZ;
+			tensor = tensor.add( //
+					tetrahedronInertiaTensor( x1, y1, z1, x2, y2, z2, x3, y3, z3 ) );
+		}
+
+		return tensor;
+	}
+
+	/**
+	 * The computations are based on <a href=
+	 * "http://docsdrive.com/pdfs/sciencepublications/jmssp/2005/8-11.pdf">this
+	 * paper</a>.
+	 * <p>
+	 * Note: In the paper b' and c' are swapped.
+	 * </p>
+	 * 
+	 * @param x1
+	 *            X coordinate of first triangle vertex
+	 * @param y1
+	 *            Y coordinate of first triangle vertex
+	 * @param z1
+	 *            Z coordinate of first triangle vertex
+	 * @param x2
+	 *            X coordinate of second triangle vertex
+	 * @param y2
+	 *            Y coordinate of second triangle vertex
+	 * @param z2
+	 *            Z coordinate of second triangle vertex
+	 * @param x3
+	 *            X coordinate of third triangle vertex
+	 * @param y3
+	 *            Y coordinate of third triangle vertex
+	 * @param z3
+	 *            Z coordinate of third triangle vertex
+	 * @return inertia tensor of this tetrahedron
+	 */
+	private static final BlockRealMatrix tetrahedronInertiaTensor( //
+			final double x1, final double y1, final double z1, //
+			final double x2, final double y2, final double z2, //
+			final double x3, final double y3, final double z3 )
+	{
+		final double volume = tetrahedronVolume( new Vector3D( x1, y1, z1 ), new Vector3D( x2, y2, z2 ),
+				new Vector3D( x3, y3, z3 ) );
+
+		final double a = 6 * volume * ( y1 * y1 + y1 * y2 + y2 * y2
+				+ y1 * y3 + y2 * y3 + y3 * y3
+				+ z1 * z1 + z1 * z2 + z2 * z2
+				+ z1 * z3 + z2 * z3 + z3 * z3 ) / 60.;
+		final double b = 6 * volume * ( x1 * x1 + x1 * x2 + x2 * x2
+				+ x1 * x3 + x2 * x3 + x3 * x3
+				+ z1 * z1 + z1 * z2 + z2 * z2
+				+ z1 * z3 + z2 * z3 + z3 * z3 ) / 60.;
+		final double c = 6 * volume * ( x1 * x1 + x1 * x2 + x2 * x2
+				+ x1 * x3 + x2 * x3 + x3 * x3
+				+ y1 * y1 + y1 * y2 + y2 * y2
+				+ y1 * y3 + y2 * y3 + y3 * y3 ) / 60.;
+
+		final double aa = 6 * volume
+				* ( 2 * y1 * z1 + y2 * z1 + y3 * z1
+						+ y1 * z2 + 2 * y2 * z2 + y3 * z2
+						+ y1 * z3 + y2 * z3 + 2 * y3 * z3 )
+				/ 120.;
+
+		final double bb = 6 * volume
+				* ( 2 * x1 * y1 + x2 * y1 + x3 * y1
+						+ x1 * y2 + 2 * x2 * y2 + x3 * y2
+						+ x1 * y3 + x2 * y3 + 2 * x3 * y3 )
+				/ 120.;
+
+		final double cc = 6 * volume
+				* ( 2 * x1 * z1 + x2 * z1 + x3 * z1
+						+ x1 * z2 + 2 * x2 * z2 + x3 * z2
+						+ x1 * z3 + x2 * z3 + 2 * x3 * z3 )
+				/ 120.;
+
+		final BlockRealMatrix t = new BlockRealMatrix( 3, 3 );
+		t.setRow( 0, new double[] { a, -bb, -cc } );
+		t.setRow( 1, new double[] { -bb, b, -aa } );
+		t.setRow( 2, new double[] { -cc, -aa, c } );
+
+		return t;
+	}
+
+	private static final double tetrahedronVolume( final Vector3D a, final Vector3D b, final Vector3D c )
+	{
+		// https://en.wikipedia.org/wiki/Tetrahedron#Volume
+		return Math.abs( a.dotProduct( b.crossProduct( c ) ) ) / 6.;
+	}
+}
