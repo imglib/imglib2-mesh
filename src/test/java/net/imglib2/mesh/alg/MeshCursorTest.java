c39449f4224a855590f19715c22003495c7fccde
Curtis Rueden <ctrueden@wisc.edu>
2023-09-28 14:34:50 -0500

    Make the MeshCursor test faster
    This reduces the size of the test cubes by 4x, which improves test
    execution time from 10s to 4s on my system. Still slow, but more
    bearable; we can tune the numbers further later as needed.

--- src/test/java/net/imglib2/mesh/alg/MeshCursorTest.java
+++ src/test/java/net/imglib2/mesh/alg/MeshCursorTest.java
@@ -59,8 +59,8 @@ public class MeshCursorTest
 	{
 		// Make an image of a cube.
 		final int expected = 100;
-		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 256, 256, 64 );
-		final FinalInterval cube = FinalInterval.createMinMax( 50, 50, 20, 150, 150, 40 );
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 128, 128, 64 );
+		final FinalInterval cube = FinalInterval.createMinMax( 50, 50, 20, 90, 90, 40 );
 		Views.interval( img, cube ).forEach( p -> p.set( ( byte ) expected ) );
 		test( img, expected );
 
@@ -70,7 +70,7 @@ public class MeshCursorTest
 	public void testSmallCube()
 	{
 		final int expected = 100;
-		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 256, 256, 64 );
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 128, 128, 64 );
 		final FinalInterval cube = FinalInterval.createMinMax( 50, 50, 20, 52, 52, 22 );
 		Views.interval( img, cube ).forEach( p -> p.set( ( byte ) expected ) );
 		test( img, expected );
@@ -81,10 +81,10 @@ public class MeshCursorTest
 	{
 		// Cube with a hole inside.
 		final int expected = 100;
-		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 256, 256, 64 );
-		final FinalInterval cube = FinalInterval.createMinMax( 50, 50, 20, 150, 150, 40 );
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 128, 128, 64 );
+		final FinalInterval cube = FinalInterval.createMinMax( 50, 50, 20, 90, 90, 40 );
 		Views.interval( img, cube ).forEach( p -> p.set( ( byte ) expected ) );
-		final FinalInterval hole = FinalInterval.createMinMax( 80, 80, 25, 120, 120, 35 );
+		final FinalInterval hole = FinalInterval.createMinMax( 60, 60, 25, 70, 70, 35 );
 		Views.interval( img, hole ).forEach( p -> p.set( ( byte ) 0 ) );
 		test( img, expected );
 	}
@@ -93,9 +93,9 @@ public class MeshCursorTest
 	public void testSphere()
 	{
 		final int expected = 100;
-		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 256, 256, 64 );
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 128, 128, 64 );
 
-		final WritableEllipsoid sphere = GeomMasks.closedEllipsoid( new double[] { 128, 128, 32 },
+		final WritableEllipsoid sphere = GeomMasks.closedEllipsoid( new double[] { 96, 96, 32 },
 				new double[] { 20, 20, 20 } );
 		Regions.sample( sphere, img ).forEach( p -> p.set( ( byte ) expected ) );
 		test( img, expected );
@@ -105,12 +105,12 @@ public class MeshCursorTest
 	public void testHollowSphere()
 	{
 		final int expected = 100;
-		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 256, 256, 64 );
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 128, 128, 64 );
 
-		final WritableEllipsoid sphere = GeomMasks.closedEllipsoid( new double[] { 128, 128, 32 },
+		final WritableEllipsoid sphere = GeomMasks.closedEllipsoid( new double[] { 96, 96, 32 },
 				new double[] { 20, 20, 20 } );
 		Regions.sample( sphere, img ).forEach( p -> p.set( ( byte ) expected ) );
-		final WritableEllipsoid hole = GeomMasks.closedEllipsoid( new double[] { 128, 128, 32 },
+		final WritableEllipsoid hole = GeomMasks.closedEllipsoid( new double[] { 96, 96, 32 },
 				new double[] { 10, 10, 10 } );
 		Regions.sample( hole, img ).forEach( p -> p.set( ( byte ) 0 ) );
 
@@ -121,9 +121,9 @@ public class MeshCursorTest
 	public void testEllipsoid()
 	{
 		final int expected = 100;
-		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 256, 256, 64 );
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 128, 128, 64 );
 
-		final WritableEllipsoid sphere = GeomMasks.closedEllipsoid( new double[] { 128, 128, 32 },
+		final WritableEllipsoid sphere = GeomMasks.closedEllipsoid( new double[] { 96, 96, 32 },
 				new double[] { 50, 50, 10 } );
 		Regions.sample( sphere, img ).forEach( p -> p.set( ( byte ) expected ) );
 		test( img, expected );
